df17621b169833928fa1f86cc7fa908118fa220a
Vickysahu12 <bikisahu161@gmail.com>
2025-09-01 15:35:57 +0530

    implement all the business logic of it

--- app/services/student_service.py
+++ app/services/student_service.py
@@ -27,3 +27,60 @@ class StudentService:
         self.next_id+=1
 
         return StudentResponse(**new_student)
+    
+    def get_all_students(self,skip:int = 0, limit:int=0,search:Optional[str] = None) -> dict:
+        """Get all Students with pagination and search"""
+        filtered_student = self.students
+
+        # Here is Our Search Functionality:-
+        if search:
+            search_lower = search_lower()
+            filtered_student = [
+                s for s in self.students
+                if search_lower in s['name'].lower() or search_lower in s['email'].lower()
+            ]
+
+            # Pagination
+            total = len(filtered_student)
+            paginated_student = filtered_student[skip:skip+limit]
+
+            return{
+                "total":total,
+                "students":[StudentResponse(**s) for s in paginated_student],
+                "page":(skip//limit)+1,
+                limit:limit
+            }
+        
+        def get_student_by_id(self,student_id:int) -> Optional[StudentResponse]:
+            """Get Students By Id"""
+            for student in self.students:
+                if student["id"] == student_id:
+                    return StudentResponse(**student)
+                return None
+            
+        def update_student(self,student_id:int,update_data:StudentUpdate) -> Optional[StudentResponse]:
+            "Update Student"
+            for i, student in enumerate(self.students):
+                if student["id"] == student_id:
+                    # Updates only Given Field
+                    update_dict = update_data.model_dump(exclude_unset=True)
+
+                    # Check email uniqueness if email is being updated
+                    if "email" in update_dict:
+                        if any(s["email"] == update_dict["email"] and s["id"] != student_id for s in self.students):
+                            raise ValueError("Email already exists")
+                        
+                    self.students[i].update(update_dict)
+                    return StudentResponse(**self.students[i])
+        return None
+                
+        def delete_student(self,student_id:int) -> bool:
+            """Delete Students"""
+            for i,student in enumerate(self.students):
+                if student['id'] == student_id:
+                    self.students.pop(i)
+                    return True
+            return False
+
+# Global service instance
+student_service = StudentService()
